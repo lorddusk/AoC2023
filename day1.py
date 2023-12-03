d34fefb801e3128fcd1a2daee5480a557d77afa4
LordDusk <lang2619@gmail.com>
2023-12-03 13:29:39 +0100

    Finished Day 1

--- day1.py
+++ day1.py
@@ -13,6 +13,7 @@ number_dict = {
     "nine": 9
 }
 
+
 def part_one(_input):
     numberList = []
     for line in _input:
@@ -23,27 +24,39 @@ def part_one(_input):
         numberList.append(int(f"{first}{last}"))
     print(f"The sum of all calibration values is: {sum(numberList)}")
 
-def part_two(_input):
-    numberList = []
-    for line in _input:
-        original_line = line
-        for item in number_dict:
-            line = line.replace(item, str(number_dict[item]))
-        numberListFromString = re.findall(r'\d+', line)
-        numberString = ''.join(numberListFromString)
-        first = numberString[0]
-        last = numberString[-1]
 
-        digit = int(f"{first}{last}")
-        print(f"{original_line}: {line}: {digit}")
-        numberList.append(digit)
-    if sum(numberList) > 53652:
-        print(f"The sum of all calibration values is: {sum(numberList)}")
-    else:
-        print(f"too low: {sum(numberList)}")
+def part_two(_input):
+    for i in range(1, 10):
+        number_dict[str(i)] = i
+
+    _sum = 0
+    for line in _input:
+        first = None
+        first_index = len(line) + 1
+        last = None
+        last_index = -1
+
+        for key, value in number_dict.items():
+            if key not in line:
+                continue
+
+            x = line.index(key)
+            y = line.rindex(key)
+
+            if x < first_index:
+                first = value
+                first_index = x
+
+            if y > last_index:
+                last = value
+                last_index = y
+
+        if first is not None and last is not None:
+            _sum += int(str(first) + str(last))
+    print(f"The sum of all calibration values is: {_sum}")
+
 
 if __name__ == "__main__":
-    stringInput = read_file(1, "string", True)
-    # part_one(stringInput)
+    stringInput = read_file(1, "string", False)
+    part_one(stringInput)
     part_two(stringInput)
-
